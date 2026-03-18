7fb8842d82a5b494588c8cf6ce3d64cc7d9125ce
cnd86038-ai <cnd86038@email.vccs.edu>
2026-03-18 15:37:38 -0400

    Setup using cmath for ceil function, include weight, distance, rate, cost, and input and output for weight of package for user.

---
+++ Chapter 4 Project Report Shipping Charges/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>  // for the ceil function
+
+using namespace std;
+
+int main() {
+	double weight, distance, rate, cost;
+	int scalefactor;
+
+	// Get the weight of the package from the user
+	cout << "Enter the weight of the package (kg): ";
+	cin >> weight;
+	}
